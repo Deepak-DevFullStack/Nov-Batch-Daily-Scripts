20d27cb68c86102d69c33db9ea2fcd19b69c76db
Priya9096 <priyakhairate0000@gmail.com>
2024-12-31 09:11:45 +0530

    ArrayMethods

--- IterationMethods.js
+++ IterationMethods.js
@@ -26,7 +26,8 @@ let odd = numbers.filter((ele)=> ele%2 !== 0 )
 console.log('Odd Elements are:',odd);//[ 11, 33, 55 ]
 
 
-//WAP to to find and print all even numbers from n array
+//WAP to to find and print all even numbers from an array
+
 //WAP to find and add all odd elements from an array into new array.
 //WAP to find sum of all elements present in an array
 //WAP to find Product of all elements present in an array.
